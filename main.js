571d3a6c2ead920387426407c6820bf0f17ce949
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2021-12-05 17:50:24 +0530

    some css and javascriptin'

--- main.js
+++ main.js
@@ -9,12 +9,20 @@ for ( i = 0; i < num ; i++)
 }
 document.getElementById('pass').textContent  = password;
 function getpass() {
-  var password= '';
-  var num = document.getElementById("len").value;
-  var alphabet =  "`1234567890-=~!@#$%^&*()_+qwertyuiop[]\\QWERTYUIOP{}|asdfgjkl;'ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>?";
-  for ( i = 0; i < num ; i++)
-{
-    password += alphabet[Math.floor((Math.random() * alphabet.length)* 1)];
-}
-  document.getElementById('pass').textContent  = password;
+    var num = document.getElementById("len").value;
+    if (num >= 9 && num <= 101){
+        var password= '';
+        var num = document.getElementById("len").value;
+        var alphabet =  "`1234567890-=~!@#$%^&*()_+qwertyuiop[]\\QWERTYUIOP{}|asdfgjkl;'ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>?";
+        for ( i = 0; i < num ; i++)
+        {   
+          password += alphabet[Math.floor((Math.random() * alphabet.length)* 1)];
+        }   
+        document.getElementById('pass').textContent  = password;
+    } else if (num < 9){
+        document.getElementById('pass').textContent  = 'Minimum acceptable value is 8';
+
+    } else if (num > 101){
+        document.getElementById('pass').textContent = 'Maximum acceptable value is 100';
+    }
 }
