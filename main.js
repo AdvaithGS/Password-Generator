64bd8e984c3f7db32091e81434a5f3597b839695
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2021-12-05 16:20:42 +0530

    Added choice and number,need to have css

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-var password = '';
-var pass = document.getElementById("pass");
-var number = 24 + Math.floor(Math.random()*10);
+document.getElementById("len").defaultValue = 24;
+document.getElementById("generate").addEventListener("click", getpass);
+var password= '';
+var num = document.getElementById("len").value;
 var alphabet =  "`1234567890-=~!@#$%^&*()_+qwertyuiop[]\\QWERTYUIOP{}|asdfgjkl;'ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>?";
-for ( i = 0; i < number ; i++)
+for ( i = 0; i < num ; i++)
 {
     password += alphabet[Math.floor((Math.random() * alphabet.length)* 1)];
 }
-pass.textContent = password;
+document.getElementById('pass').textContent  = password;
+function getpass() {
+  var password= '';
+  var num = document.getElementById("len").value;
+  var alphabet =  "`1234567890-=~!@#$%^&*()_+qwertyuiop[]\\QWERTYUIOP{}|asdfgjkl;'ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>?";
+  for ( i = 0; i < num ; i++)
+{
+    password += alphabet[Math.floor((Math.random() * alphabet.length)* 1)];
+}
+  document.getElementById('pass').textContent  = password;
+}
