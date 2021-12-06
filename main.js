12f4d319945b338f07da59ca25b015a3e8d9c4a0
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2021-12-06 13:12:41 +0530

    changes in js and css

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ for ( i = 0; i < num ; i++)
 document.getElementById('pass').textContent  = password;
 function getpass() {
     var num = document.getElementById("len").value;
-    if (num >= 9 && num <= 101){
+    if (num >= 8 && num <= 100){
         var password= '';
         var num = document.getElementById("len").value;
         var alphabet =  "`1234567890-=~!@#$%^&*()_+qwertyuiop[]\\QWERTYUIOP{}|asdfgjkl;'ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>?";
@@ -19,10 +19,10 @@ function getpass() {
           password += alphabet[Math.floor((Math.random() * alphabet.length)* 1)];
         }   
         document.getElementById('pass').textContent  = password;
-    } else if (num < 9){
+    } else if (num < 8){
         document.getElementById('pass').textContent  = 'Minimum acceptable value is 8';
 
-    } else if (num > 101){
+    } else if (num > 100){
         document.getElementById('pass').textContent = 'Maximum acceptable value is 100';
     }
 }
